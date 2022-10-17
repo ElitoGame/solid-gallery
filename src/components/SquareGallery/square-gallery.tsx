1d99fe8aa29c3967db99a2b992c8ae63ba0a4588
ElitoGame <elitogame123@gmail.com>
2022-10-17 18:07:20 +0200

    Minor Square gallery update
    - Added default values to the SquareGalleryProps.

--- src/components/SquareGallery/square-gallery.tsx
+++ src/components/SquareGallery/square-gallery.tsx
@@ -1,52 +1,69 @@
-import { children, createEffect, createSignal, For, JSXElement } from "solid-js";
+import { children, createEffect, createSignal, For, JSXElement, mergeProps } from "solid-js";
 import { SquareGalleryProps } from "../types";
 
 export const SquareGallery = (props: SquareGalleryProps) => {
+  const merged = mergeProps(
+    {
+      squareSize: "20vw",
+      minSquareSize: "200px",
+      maxSquareSize: "400px",
+      squareGap: "1rem",
+    },
+    props,
+  );
 
-    const c = children(() => props.children);
+  const c = children(() => props.children);
 
-    const [images, setImages] = createSignal<JSXElement[]>();
+  const [images, setImages] = createSignal<JSXElement[]>();
 
-
-    // Make sure all images have a object-fit: cover
-    createEffect(() => {
-        let elements: JSXElement[] = [];
-        if (Array.isArray(c())) {
-            elements = c() as JSXElement[];
+  // Make sure all images have a object-fit: cover
+  createEffect(() => {
+    let elements: JSXElement[] = [];
+    if (Array.isArray(c())) {
+      elements = c() as JSXElement[];
+    } else {
+      elements = [c() as JSXElement];
+    }
+    setImages(
+      elements?.map((jsxEl) => {
+        if (jsxEl instanceof HTMLImageElement) {
+          let el = jsxEl as HTMLImageElement;
+          el.style.objectFit = "cover";
+          return el as JSXElement;
         } else {
-            elements = [c() as JSXElement];
+          return jsxEl;
         }
-        setImages(elements?.map((jsxEl) => {
-            if (jsxEl instanceof HTMLImageElement) {
-                let el = (jsxEl as HTMLImageElement);
-                el.style.objectFit = "cover"
-                return el as JSXElement;
-            } else {
-                return jsxEl;
-            }
-        }))
-    });
-
-    return (
-        <div style={{
-            display: "flex",
-            "flex-wrap": "wrap",
-            gap: props.squareGap,
-            "justify-content": "center",
-            "max-width": "90%",
-        }}>
-            <For each={images()} fallback={<div>No items</div>}>
-                {(item, index) => <div data-index={index()} style={{
-                    "width": props.squareSize,
-                    "height": props.squareSize,
-                    "min-width": props.minSquareSize,
-                    "min-height": props.minSquareSize,
-                    "max-width": props.maxSquareSize,
-                    "max-height": props.maxSquareSize,
-                }}>
-                    {item}
-                </div>}
-            </For>
-        </div>
+      }),
     );
+  });
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        "flex-wrap": "wrap",
+        gap: props.squareGap,
+        "justify-content": "center",
+        "max-width": "90%",
+      }}
+    >
+      <For each={images()} fallback={<div>No items</div>}>
+        {(item, index) => (
+          <div
+            data-index={index()}
+            style={{
+              width: merged.squareSize,
+              height: merged.squareSize,
+              "min-width": merged.minSquareSize,
+              "min-height": merged.minSquareSize,
+              "max-width": merged.maxSquareSize,
+              "max-height": merged.maxSquareSize,
+            }}
+          >
+            {item}
+          </div>
+        )}
+      </For>
+    </div>
+  );
 };
